4567a4cdb2434ececa95e6e0184d91a57eef0b8a
kaprowl <aucarapon.m1@gmail.com>
2023-03-04 21:50:33 +0700

    gegregreg

--- web/app/views.py
+++ web/app/views.py
@@ -1,11 +1,18 @@
 from flask import (jsonify, render_template,
-                   request, url_for, flash, redirect)
+                   request, url_for, flash, redirect,
+                    Flask, abort)
 import json
+import os
+import pathlib
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
 from sqlalchemy.sql import text
 from flask_login import login_user, login_required, logout_user, current_user
-
 from app import app
 from app import db
 from app import login_manager
@@ -13,6 +20,26 @@ from app import login_manager
 from app.models.contact import Contact
 from app.models.BlogEntry import BlogEntry
 from app.models.authuser import AuthUser, PrivateContact , PrivateBlogEntry
+
+app.secret_key = "1234"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GoogleClientId = "461997807646-tpggvset01g4uv6rnmkqvpstjdqf9r7d.apps.googleusercontent.com"
+
+client_secret_file= os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secret_file=client_secret_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://localhost:56733/callback")
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)
+        else:
+            return function()
+    return wrapper
 
 @app.route('/')
 def home():
@@ -439,3 +466,35 @@ def lab12_logout():
     logout_user()
     return redirect(url_for('lab12_index'))
     
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GoogleClientId
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/lab12/profile")
+
+@app.route("/logout")
+def logout():
+    pass 
+
